bff026356b7a54b38d7dbb7454eea5c69f91e4ff
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-17 13:58:19 -0500

    instalamos react, svelte y tailwind, ademas de iconos para astro react y svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import svelte from '@astrojs/svelte';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), svelte(), icon()]
+});
