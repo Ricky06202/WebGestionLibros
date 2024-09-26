3fa50e02f4ae7f417aabb54aa86351df06c28ac7
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-26 12:05:12 -0500

    desactivamos la devtoolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), svelte(), icon()]
+  integrations: [react(), tailwind(), svelte(), icon()],
+  devToolbar: { enabled: false },
 });
+
