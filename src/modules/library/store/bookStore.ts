476738dd721fc15a6dcd5bcf75b607ea4656e8ff
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-01 12:10:49 -0500

    arreglamos el bookstore para que funcione con las nanostores

--- src/modules/library/store/bookStore.ts
+++ src/modules/library/store/bookStore.ts
@@ -1,18 +1,5 @@
 import type { libro } from "@library/constants/ApiLibrosTypes";
-import { create } from "zustand";
-interface booksState {
-  bookListSize: number;
-  setBookListSize: (size: number) => void;
-  bookList: libro[];
-  setBookList: (bookList: libro[]) => void;
-}
-const useBookStore = create<booksState>()((set) => ({
-  bookListSize: 1,
-  setBookListSize: (size: number) => {
-    set({ bookListSize: size });
-  },
-  bookList: [],
-  setBookList: (bookList: libro[]) => {
-    set({ bookList });
-  },
-}));
+import { atom } from "nanostores";
+
+export const bookListSize = atom(1);
+export const bookList = atom<libro[]>([]);
