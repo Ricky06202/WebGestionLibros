092acda92d5628a7a693dc1963cfc55802977170
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-01 10:58:16 -0500

    hicimos la store para los estados globales con zustand

--- src/modules/library/store/bookStore.ts
+++ src/modules/library/store/bookStore.ts
@@ -1,5 +1,18 @@
-interface bookState {
-  lista: any[];
-  setLista: any;
+import type { libro } from "@library/constants/ApiLibrosTypes";
+import { create } from "zustand";
+interface booksState {
+  bookListSize: number;
+  setBookListSize: (size: number) => void;
+  bookList: libro[];
+  setBookList: (bookList: libro[]) => void;
 }
-const useBookStore = create<bookState>((set) => ({}));
+const useBookStore = create<booksState>()((set) => ({
+  bookListSize: 1,
+  setBookListSize: (size: number) => {
+    set({ bookListSize: size });
+  },
+  bookList: [],
+  setBookList: (bookList: libro[]) => {
+    set({ bookList });
+  },
+}));
